61fae5acf4bca136e87559daeaa9f14dd2281afa
Aleksandar Stankovic <stankovic.aleksandar@elfak.rs>
2020-05-28 22:28:32 +0200

    fix(routes): change return format

--- src/routes/category_routes.js
+++ src/routes/category_routes.js
@@ -13,18 +13,18 @@ router.delete('/:id', asyncMiddleware(deleteCategory));
 async function getAllCategories(req, res) {
   const { limit, skip, sort } = req.query;
   const categories = await CategoryRepository.getAll({ limit, skip, sort });
-  return res.json({ categories });
+  return res.json(categories);
 }
 
 async function createCategory(req, res) {
   const category = await CategoryRepository.create(req.body);
-  return res.json({ category });
+  return res.json(category);
 }
 
 async function updateCategory(req, res) {
   const { id } = req.params;
   const category = await CategoryRepository.update(id, req.body);
-  return res.json({ category });
+  return res.json(category);
 }
 
 
--- src/routes/cinema_routes.js
+++ src/routes/cinema_routes.js
@@ -12,18 +12,18 @@ router.delete('/:id', asyncMiddleware(deleteCinema));
 async function getAllCinemas(req, res) {
   const { limit, skip, sort } = req.query;
   const cinemas = await CinemaRepository.getAll({ limit, skip, sort });
-  return res.json({ cinemas });
+  return res.json(cinemas);
 }
 
 async function createCinema(req, res) {
   const cinema = await CinemaRepository.create(req.body);
-  return res.json({ cinema });
+  return res.json(cinema);
 }
 
 async function updateCinema(req, res) {
   const { id } = req.params;
   const cinema = await CinemaRepository.update(id, req.body);
-  return res.json({ cinema });
+  return res.json(cinema);
 }
 
 async function deleteCinema(req, res) {
