84bb761d3de4d90e3e915544e8f817bbce432e67
Aleksandar Stankovic <stankovic.aleksandar@elfak.rs>
2020-05-30 22:39:56 +0200

    fix(projection seeder): seed multiple times for movie

--- src/database/seeders/projection_seeder.js
+++ src/database/seeders/projection_seeder.js
@@ -10,16 +10,25 @@ const { ProjectionRepository, CinemaRepository, MovieRepository } = require('../
   try {
     const movies = await MovieRepository.getAll();
     const cinemas = await CinemaRepository.getAll();
+    let created = 0;
     // eslint-disable-next-line no-restricted-syntax
     for (const movie of movies) {
       for (let i = 0; i < cinemas.length; i++) {
-        const hallNumber = Math.floor(Math.random() * 6) + 1;
-        const castTime = moment().add(Math.random() * 200, 'hour').format('YYYY-MM-DD HH:mm:ss');
-        const projectionBody = {
-          hall: hallNumber, movieId: movie.id, name: movie.title, numberOfSeats: 100, seatsAvailable: 100, time: castTime,
-        };
-        await ProjectionRepository.createProjection(movie.id, cinemas[i].id, projectionBody);
+        const createMany = Math.floor((Math.random() * 3) + 1);
+        const projections = [];
+        for (let createI = 0; createI < createMany; createI++) {
+          const hallNumber = Math.floor(Math.random() * 10) + 1;
+          const castTime = moment().add(Math.random() * 300, 'hour').format('YYYY-MM-DD HH:mm:ss');
+          const seats = Math.floor(Math.random() * 50 + 50);
+          const projectionBody = {
+            hall: hallNumber, movieId: movie.id, name: movie.title, numberOfSeats: seats, seatsAvailable: seats, time: castTime,
+          };
+          projections.push(projectionBody);
+        }
+        created += projections.length;
+        await Promise.all(projections.map((projection) => ProjectionRepository.createProjection(movie.id, cinemas[i].id, projection)));
       }
+      console.log(`Created ${created} projections.`);
     }
   } catch (error) {
     console.log(error);
